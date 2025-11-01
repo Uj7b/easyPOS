7bfd6a2f7e708e6576573bd304a3e27c4cbbffaa
unknown <Uj7b>
2025-11-01 19:37:04 +0100

    category filter upgate

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import Header from "./Header";
 import SideMenu from "./SideMenu";
 import NotificationsTab from "./NotificationsTab";
 import ProductGrid from "./ProductGrid";
+import CategoryFilter from "./CategoryFilter";
 
 function App() {
   // const [order, setOrder] = useState([]);
   return (
-    <div className="">
-      <Header></Header>
-      <ProductGrid></ProductGrid>
-      </div>
+    <CategoryFilter></CategoryFilter>
   );
 }
 
--- src/CategoryFilter.jsx
+++ src/CategoryFilter.jsx
@@ -1,5 +1,25 @@
+import { useEffect, useState } from "react";
+import Menu from "./menu.json";
 function CategoryFilter() {
-    return ("thi is the tab where order categories shows up");
+  const [categories, setCategries] = useState([]);
+  const [activeCategory, setActiveCategory] = useState("Bakeries");
+  const [activeTab, setActiveTab] = useState("All");
+  useEffect(() => {
+    setCategries(Menu.menu);
+  }, [activeCategory]);
+  const active = categories.find((c) => c.category === activeCategory);
+  return (
+    <div className="flex gap-2.5">
+      <button className="cursor-pointer px-5 py-1.5 text-orange-500 bg-orange-200 w-fit rounded-full border border-orange-500">
+        All
+      </button>
+      {active?.subcategories?.map((sub) => (
+        <button className="cursor-pointer px-5 py-1.5 text-orange-500 bg-orange-200 w-fit rounded-full border border-orange-500">
+          {sub.name}
+        </button>
+      ))}
+    </div>
+  );
 }
 
 export default CategoryFilter;
