53e6bfa08e9292e911adcede889ce1206eb2bc6f
Uj7b <107117746+Uj7b@users.noreply.github.com>
2025-11-03 12:19:29 +0100

    grid layout fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,44 @@ import SideMenu from "./SideMenu";
 import NotificationsTab from "./NotificationsTab";
 import ProductGrid from "./ProductGrid";
 import CategoryFilter from "./CategoryFilter";
-
+import Invoice from "./Invoice";
 function App() {
   // const [order, setOrder] = useState([]);
   return (
-    <>
-      <Header />
-      <SideMenu />
-      <CategoryFilter />
-      <ProductGrid/>
-    </>
+    <div
+      className="grid h-screen 
+      grid-cols-[auto_auto_auto] 
+      grid-rows-[80px_auto_1fr] 
+      grid-areas-[ 
+        'header header header' 
+        'sidebar filter invoice' 
+        'sidebar product-grid invoice' 
+      ]"
+      style={{
+        display: "grid",
+        gridTemplateAreas: `
+          "header header header"
+          "sidebar filter invoice"
+          "sidebar product-grid invoice"
+        `,
+      }}
+    >
+      <div style={{ gridArea: "header" }}>
+        <Header />
+      </div>
+      <div style={{ gridArea: "sidebar" }}>
+        <SideMenu />
+      </div>
+      <div style={{ gridArea: "filter" }}>
+        <CategoryFilter />
+      </div>
+      <div style={{ gridArea: "product-grid" }}>
+        <ProductGrid />
+      </div>
+      <div style={{ gridArea: "invoice" }}>
+        <Invoice></Invoice>
+      </div>
+    </div>
   );
 }
 
