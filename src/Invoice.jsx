ec87946e40877f18c74730f4057c4a97f3e4d545
Uj7b <107117746+Uj7b@users.noreply.github.com>
2025-11-05 22:37:40 +0100

    05/11/2025

--- src/Invoice.jsx
+++ src/Invoice.jsx
@@ -3,29 +3,30 @@ import PrintInvoiceButton from "./Buttons/PrintInvoiceButton";
 
 function Invoice() {
   return (
-    <div className="w-60 bg-cyan-500">
-      <span>
+    <div className="w-60 bg-white rounded-2xl p-4">
+      <span className="text-[#90939B] text-[14px]">
         Invoice No: 123454 {new Date().toLocaleDateString("en-US")}
         {" | "}
         {new Date().toLocaleTimeString("en-US", { hour12: false })}
       </span>
       <hr className="border-t border-dotted border-gray-400" />
       <div>LOGO GOES HERE</div>
-      <div>
-        table 04
-        order: #0029
+      <div className="flex justify-between">
+        <span className="text-[#FF5733]">table 04</span>
+        <span>order: #0029</span>
       </div>
-      <div>Order 1</div><hr />
-      <div>Order 2</div><hr />
-      <div>Order 3</div><hr />
-      <div>Order 4</div><hr />
-      <hr/>
-      <div>
-        Total
-        items quantity $77
-      </div>
-      <PrintInvoiceButton/>
-      <PaymentButton/>
+      <div>Order 1</div>
+      <hr />
+      <div>Order 2</div>
+      <hr />
+      <div>Order 3</div>
+      <hr />
+      <div>Order 4</div>
+      <hr />
+      <hr />
+      <div>Total items quantity $77</div>
+      <PrintInvoiceButton />
+      <PaymentButton />
     </div>
   );
 }
