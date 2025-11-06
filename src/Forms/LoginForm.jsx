d7362aae0098b528c2466b657c5447e5f400312b
Uj7b <107117746+Uj7b@users.noreply.github.com>
2025-11-06 23:24:12 +0100

    06/11/2025

--- src/Forms/LoginForm.jsx
+++ src/Forms/LoginForm.jsx
@@ -1,8 +1,11 @@
 function LoginForm() {
     let login = "Login"
+    function handleLogin () {
+
+    }
     return ( 
         <div>
-            <form> 
+            <form onSubmit={handleLogin}> 
                 <input type="text"/>
                 <input type="password"/>
                 <input type="submit" value={login} />
